b9eb7b8cf342c4263b702a502a1e27fec98f345f
Meghan McLaughlin <meghan-l-mclaughlin@uiowa.edu>
2022-07-14 21:59:45 -0700

    updated to output graph as png

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # visual-pokemon-types
-Repo for creating visuals of Pokemon type effectiveness.
+Repo for creating visuals of Pokemon type effectiveness, currently using [Graphviz](https://graphviz.org/)
+
+![Generated Image](out/type-chart.png)
--- src/type-chart.py
+++ src/type-chart.py
@@ -21,11 +21,10 @@ graph_args = {
     'layout': 'circo',
     'label': graph_title,
     'fontname': "Helvetica,Arial,sans-serif",
-    'mindist': '3'  # used for circo, specifies the minimum distance between nodes.
+    'mindist': '2'  # used for circo, specifies the minimum distance between nodes.
 }
 
 graph = graphviz.Digraph(graph_title, node_attr=node_args, graph_attr=graph_args)
-
 
 # Make Nodes
 for poke_type in poke_types['PokeTypes']:
@@ -48,7 +47,6 @@ for poke_type in poke_types['PokeTypes']:
     for no_effect in poke_type["no-effect"]:
         graph.edge(poke_type["ref"], no_effect, color='grey')
 
-# Write .gv file
-f_out = open("../out/type-chart.gv", "w")
-f_out.write(graph.source)
-f_out.close()
+# Render as .png
+# Also writes the source .gv file
+graph.render(format='png', outfile='../out/type-chart.png').replace('\\', '/')
